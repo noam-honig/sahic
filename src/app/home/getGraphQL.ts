f1794da60c540aa07b94c83ad0b4409cea614084
Noam Honig <noam.honig@gmail.com>
2023-12-27 07:53:21 +0200

    updated api version

--- src/app/home/getGraphQL.ts
+++ src/app/home/getGraphQL.ts
@@ -1,17 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
-export async function gql( s: string,variables: any) {
-    return (await axios.post('https://api.monday.com/v2',
-        JSON.stringify({
-            "query": s,
-            "variables": variables
-        })
-        , {
-            headers: {
-                'authorization': process.env["MONDAY_API_TOKEN"]!,
-                'content-type': 'application/json',
-            },
-        })).data.data;
-
-
+export async function gql(s: string, variables: any) {
+  const r = await axios.post(
+    'https://api.monday.com/v2',
+    JSON.stringify({
+      query: s,
+      variables: variables,
+    }),
+    {
+      headers: {
+        authorization: process.env['MONDAY_API_TOKEN']!,
+        'content-type': 'application/json',
+        'API-Version': '2023-10',
+      },
+    }
+  );
+  if (!r.data.data) {
+    console.log(r.data);
+  }
+  return r.data.data;
 }
--- src/app/home/home.controller.ts
+++ src/app/home/home.controller.ts
@@ -1,46 +1,62 @@
-import { BackendMethod, Remult } from "remult";
-import { gql } from "./getGraphQL";
+import { BackendMethod, Remult } from 'remult';
+import { gql } from './getGraphQL';
 
 export class HomeController {
   @BackendMethod({ allowed: true })
   static async getLinks(id: number) {
-    const first = await gql(`query ($id: Int!) {
+    const first = await gql(
+      `#graphql
+      query ($id: ID!) {
             boards(ids: [3087291764]) {
               id
               name
-              items (ids: [$id]){
+              columns{
+                id
+                title
+              }
+              items_page(query_params: {ids: [$id]},limit:1){
+              items {
                 id
                 name
                 column_values{
                   id
-                  title
                   value
                 }
                 subitems {
                   name
                   column_values {
                     id
-                    title
                     value
                   }
                 }
-              }
+              }}
             }
-          }`, {
-      id: id
-    });
-    const r: LinksResult = first.boards[0].items[0];
+          }`,
+      {
+        id: id,
+      }
+    );
+    const r: LinksResult = first.boards[0].items_page.items[0];
+    const columns: { id: string; title: string }[] = first.boards[0].columns;
 
     if (r?.column_values)
-      r.column_values = r.column_values.filter(x => x.value?.toLocaleLowerCase().startsWith("\"https")).map(y => ({ ...y, value: JSON.parse(y.value) }));
+      r.column_values = r.column_values
+        .filter((x) => x.value?.toLocaleLowerCase().startsWith('"https'))
+        .map((y: any) => ({
+          ...y,
+          title: columns.find((c) => c.id == y.id)?.title || '',
+          value: JSON.parse(y.value),
+        }));
     if (r.subitems) {
       for (const s of r.subitems) {
-        let link = s.column_values.find(x => x.value?.toLocaleLowerCase().startsWith("\"https"))
+        let link = s.column_values.find((x) =>
+          x.value?.toLocaleLowerCase().startsWith('"https')
+        );
         if (link)
           r.column_values.push({
             title: s.name,
-            value: JSON.parse(link.value)
-          })
+            value: JSON.parse(link.value),
+          });
       }
     }
     delete r.subitems;
@@ -49,13 +65,14 @@ export class HomeController {
   @BackendMethod({ allowed: true })
   static async getTeenagers() {
     const result: {
-      name: string,
+      name: string;
       items: {
-        id: number,
-        name: string,
-        attended?: boolean
-      }[]
-    } = await gql(`query  {
+        id: number;
+        name: string;
+        attended?: boolean;
+      }[];
+    } = await gql(
+      `query  {
       boards (ids:[1363121136]){
         id
         name
@@ -70,14 +87,15 @@ export class HomeController {
        
         
       }
-    }`, {}).then(x => x.boards[0]);
+    }`,
+      {}
+    ).then((x) => x.boards[0]);
     return result;
   }
-
 }
 
 export interface LinksResult {
-  name: string,
-  column_values: { title: string, value: string }[]
-  subitems?: LinksResult[]
+  name: string;
+  column_values: { title: string; value: string }[];
+  subitems?: LinksResult[];
 }
