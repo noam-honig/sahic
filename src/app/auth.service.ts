ff39c19247bbf66c9e765ce8490d088aeffe9e6e
Noam Honig <noam.honig@gmail.com>
2024-04-04 15:28:57 +0300

    cleanup

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,35 +3,34 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { Remult } from 'remult';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-    constructor(private remult: Remult) {
-        const token = AuthService.fromStorage();
-        if (token) {
-            this.setAuthToken(token);
-        }
+  constructor(private remult: Remult) {
+    const token = AuthService.fromStorage();
+    if (token) {
+      this.setAuthToken(token);
     }
+  }
 
-    setAuthToken(token: string | null, rememberOnThisDevice = false) {
-        if (token) {
-            this.remult.setUser(new JwtHelperService().decodeToken(token));
-            sessionStorage.setItem(AUTH_TOKEN_KEY, token);
-            if (rememberOnThisDevice) {
-                localStorage.setItem(AUTH_TOKEN_KEY, token);
-            }
-
-        }
-        else {
-            this.remult.setUser(undefined!);
-            sessionStorage.removeItem(AUTH_TOKEN_KEY);
-            localStorage.removeItem(AUTH_TOKEN_KEY);
-        }
+  setAuthToken(token: string | null, rememberOnThisDevice = false) {
+    if (token) {
+      this.remult.setUser(new JwtHelperService().decodeToken(token));
+      //     sessionStorage.setItem(AUTH_TOKEN_KEY, token);
+      if (rememberOnThisDevice) {
+        //   localStorage.setItem(AUTH_TOKEN_KEY, token);
+      }
+    } else {
+      this.remult.setUser(undefined!);
+      //   sessionStorage.removeItem(AUTH_TOKEN_KEY);
+      //    localStorage.removeItem(AUTH_TOKEN_KEY);
     }
+  }
 
-    static fromStorage(): string {
-        return sessionStorage.getItem(AUTH_TOKEN_KEY) || localStorage.getItem(AUTH_TOKEN_KEY)!;
-    }
+  static fromStorage(): string {
+    return '';
+    //return sessionStorage.getItem(AUTH_TOKEN_KEY) || localStorage.getItem(AUTH_TOKEN_KEY)!;
+  }
 }
 
-const AUTH_TOKEN_KEY = "authToken";
+const AUTH_TOKEN_KEY = 'authToken';
